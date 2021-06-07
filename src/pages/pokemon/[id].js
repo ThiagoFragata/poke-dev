cfc9fd829cfe7fc2f8886e62c3205d27f20ab438
thiago henrique fragata <thiago.henrique.fragata@hotmail.com>
2021-06-07 02:07:22 -0400

    style for pageHome

--- src/pages/pokemon/[id].js
+++ src/pages/pokemon/[id].js
@@ -42,7 +42,8 @@ export async function getStaticProps({ params }) {
     return {
         props: {
             pokemon,
-        }, // will be passed to the page component as props
+        },
+        // will be passed to the page component as props
     };
 }
 
